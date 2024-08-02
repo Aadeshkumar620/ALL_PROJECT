562fac530cdbefa29370ffd1dc119b16521ab363
Aadesh kumar <162555223+Aadeshkumar620@users.noreply.github.com>
2024-08-02 11:10:51 +0000

    commit

--- 8_GAMECHENGER/script.js
+++ 8_GAMECHENGER/script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function(dets){
+        console.log(dets);
+    })
+
+}
+circleMouseFollower();
